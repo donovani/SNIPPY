e3b14d8962b0dfdccdc2fdb5026264786ab6c36f
Ian Donovan <epicpunnum@ymail.com>
2017-03-09 01:41:46 -0500

    Fixed MainScene,  redid base layout, and added error handling to UXUtils

--- application/src/io/snippy/main/MainScene.java
+++ application/src/io/snippy/main/MainScene.java
@@ -1,9 +1,16 @@
 package io.snippy.main;
 
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import io.snippy.core.StageScene;
 import io.snippy.util.UXUtils;
+import javafx.animation.Transition;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.beans.EventHandler;
 
 
 /**
@@ -12,18 +19,27 @@ import javafx.stage.Stage;
  */
 public class MainScene extends StageScene {
 
+	private JFXHamburger menuButton;
+	private HamburgerBasicCloseTransition closeTransition;
+
 	public MainScene( Stage primaryStage ) {
 		super( primaryStage );
 	}
 
 	@Override
 	public Parent inflateLayout() {
-		return UXUtils.inflate( "assets/layout/main_base.fxml" );
+		return UXUtils.inflate( "assets/layouts/base_layout.fxml" );
 	}
 
 	@Override
 	public void onCreate() {
-
+		menuButton = (JFXHamburger) this.lookup( "#base_menu" );
+		closeTransition = new HamburgerBasicCloseTransition( menuButton );
+		closeTransition.setRate( -3 );
+		menuButton.addEventHandler( MouseEvent.MOUSE_PRESSED, (e)->{
+			closeTransition.setRate(closeTransition.getRate() * -1);
+			closeTransition.play();
+		});
 	}
 
 	@Override
