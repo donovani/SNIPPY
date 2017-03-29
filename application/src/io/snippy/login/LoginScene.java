914c94a207c5ca740645cb70208c072dd325f9f1
Jake Hall <hallja99@gmail.com>
2017-03-29 17:48:17 -0400

    Login working - Added login(username, pass) method to SQLUtils - Implemented login functionality

--- application/src/io/snippy/login/LoginScene.java
+++ application/src/io/snippy/login/LoginScene.java
@@ -1,13 +1,20 @@
 package io.snippy.login;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import io.snippy.core.StageScene;
 import io.snippy.main.MainScene;
 import io.snippy.recover.RecoverEmailScene;
 import io.snippy.register.RegisterScene;
+import io.snippy.util.SQLUtils;
 import io.snippy.util.UXUtils;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 
 /**
@@ -16,27 +23,41 @@ import javafx.stage.Stage;
  */
 public class LoginScene extends StageScene {
 
-	public LoginScene( Stage stage ) {
-		super( stage );
-	}
+    public LoginScene(Stage stage) {
+        super(stage);
+    }
 
-	@Override
-	public Parent inflateLayout() {
-		return UXUtils.inflate( "assets/layouts/layout_login.fxml" );
-	}
+    @Override
+    public Parent inflateLayout() {
+        return UXUtils.inflate("assets/layouts/layout_login.fxml");
+    }
 
-	@Override
-	public void onCreate() {
-		JFXButton loginButton = (JFXButton) lookup( "#login_submit" );
-		loginButton.setOnAction( event -> switchScreen( MainScene.class ) );
-		JFXButton registerButton = (JFXButton) lookup( "#login_register" );
-		registerButton.setOnAction( event -> switchScreen( RegisterScene.class ) );
-		JFXButton forgotButton = (JFXButton) lookup( "#login_forgot" );
-		forgotButton.setOnAction( event -> switchScreen( RecoverEmailScene.class ) );
-	}
+    @Override
+    public void onCreate() {
+        JFXButton loginButton = (JFXButton) lookup("#login_submit");
 
-	@Override
-	public void onDispose() {
+        loginButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                String username = ((JFXTextField) lookup("#login_email")).getText();
+                String password = ((JFXPasswordField) lookup("#login_password")).getText();
 
-	}
+                if (SQLUtils.login(username, password)) {
+                    switchScreen(MainScene.class);
+                } else {
+                    lookup("#login_error").setVisible(true);
+                }
+            }
+        });
+
+        JFXButton registerButton = (JFXButton) lookup("#login_register");
+        registerButton.setOnAction(event -> switchScreen(RegisterScene.class));
+        JFXButton forgotButton = (JFXButton) lookup("#login_forgot");
+        forgotButton.setOnAction(event -> switchScreen(RecoverEmailScene.class));
+    }
+
+    @Override
+    public void onDispose() {
+
+    }
 }
