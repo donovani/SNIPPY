97920f8722d1d3fc47d5f4330e0110cae5975201
Ian Donovan <epicpunnum@ymail.com>
2017-03-31 09:51:23 -0400

    Added DeleteDialog to the MainScene as an example dialog
    Had to rework the main layout too to add a StackPane so that the overlay would work.

--- application/src/io/snippy/main/MainScene.java
+++ application/src/io/snippy/main/MainScene.java
@@ -1,14 +1,17 @@
 package io.snippy.main;
 
-import com.jfoenix.controls.JFXHamburger;
-import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.*;
 import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import io.snippy.core.R;
 import io.snippy.core.StageScene;
 import io.snippy.util.UXUtils;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.pmw.tinylog.Logger;
 
 
 /**
@@ -43,11 +46,53 @@ public class MainScene extends StageScene {
 			closeTransition.setRate(closeTransition.getRate() * -1);
 			closeTransition.play();
 		});
+
+		StackPane overlay = (StackPane) this.lookup( "#base_stack" );
+		JFXButton deleteButton = (JFXButton) this.lookup( "#main_delete" );
+		String snipID = "<snip uuid goes here>";
+		Logger.info( "Creating deletion dialog for SnipID {}", snipID);
+		deleteButton.setOnAction( event -> DeleteDialog.createAndShow( overlay, snipID ) );
+
+		//Lastly we load the data of the app
 		JFXListView< SnipListItem > snips = (JFXListView< SnipListItem >) lookup( "#base_selections" );
 	}
 
 	@Override
 	public void onDispose() {
 
+	}
+}
+
+class DeleteDialog extends JFXDialog {
+
+	private final StackPane stackPane;
+	private final String snipID;
+	private JFXDialogLayout layout;
+	private JFXButton cancelButton, confirmButton;
+
+	private DeleteDialog( StackPane stackPane, String snipID ) {
+		this.stackPane = stackPane;
+		this.snipID = snipID;
+		this.layout = new JFXDialogLayout();
+		create( );
+	}
+
+	private final void create( ) {
+		cancelButton = new JFXButton( R.strings( "common.cancel" ) );
+		confirmButton = new JFXButton( R.strings( "delete.confirm" ) );
+		confirmButton.setStyle( "-fx-text-fill: WHITE; -fx-background-color: RED;" );
+
+		layout.setHeading( new Label( R.strings( "delete.header" ) ) );
+		layout.setBody( new Label( R.strings( "delete.body" ) ) );
+		layout.setActions( cancelButton, confirmButton );
+
+		this.setTransitionType( DialogTransition.CENTER );
+		this.setContent( layout );
+	}
+
+	public static final JFXDialog createAndShow( StackPane sp, String id ) {
+		JFXDialog d = new DeleteDialog( sp, id );
+		d.show( sp );
+		return d;
 	}
 }
