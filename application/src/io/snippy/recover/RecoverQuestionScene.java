6064f0839523e88d7b9448d7d3b517e49a7aada7
Jake Hall <hallja99@gmail.com>
2017-04-03 03:54:32 -0400

    Implemented Security Questions
    - implemented the ability to answer security questions on recovery is user has them

--- application/src/io/snippy/recover/RecoverQuestionScene.java
+++ application/src/io/snippy/recover/RecoverQuestionScene.java
@@ -1,11 +1,18 @@
 package io.snippy.recover;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import io.snippy.core.StageScene;
 import io.snippy.login.LoginScene;
+import io.snippy.util.SQLUtils;
 import io.snippy.util.UXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.Arrays;
 
 
 /**
@@ -13,27 +20,77 @@ import javafx.stage.Stage;
  * Created by Ian on 3/1/2017.
  */
 public class RecoverQuestionScene extends StageScene {
+    private static int qNum = 1;
+    private static String[] user = null;
 
-	public RecoverQuestionScene( Stage primaryStage ) {
-		super( primaryStage );
-	}
+    public RecoverQuestionScene(Stage primaryStage) {
+        super(primaryStage);
+    }
 
-	@Override
-	public Parent inflateLayout() {
-		return UXUtils.inflate( "assets/layouts/layout_recover2.fxml" );
-	}
+    @Override
+    public Parent inflateLayout() {
+        return UXUtils.inflate("assets/layouts/layout_recover2.fxml");
+    }
 
-	@Override
-	public void onCreate() {
+    @Override
+    public void onCreate() {
+        if (user == null) { //generate a user
+            String tmp = SQLUtils.getUser(SQLUtils.getUserID(RecoverEmailScene.username));
+            user = tmp.split("`");
+        }
 
-		JFXButton cancelButton = (JFXButton) lookup( "#recover_cancel" );
-		cancelButton.setOnAction( event -> switchScreen( LoginScene.class ) );
-		JFXButton nextButton = (JFXButton) lookup( "#recover_next" );
-		nextButton.setOnAction( event -> switchScreen( RecoverCodeScene.class ) );
-	}
+        if (!user[3].equals("null") || !user[5].equals("null")) {// if the user did security questions
+            JFXButton cancelButton = (JFXButton) lookup("#recover_cancel");
+            cancelButton.setOnAction(event -> switchScreen(LoginScene.class));
 
-	@Override
-	public void onDispose() {
+            if (user[3].equals("null")) { //if user only selected second question, use that
+                qNum = 2;
+            }
 
-	}
+            if (qNum == 1) { //question 1
+                ((Text) lookup("#recover_question")).setText(user[3]);
+            } else {//question 2
+                ((Text) lookup("#recover_question")).setText(user[5]);
+            }
+
+
+            JFXButton nextButton = (JFXButton) lookup("#recover_next");
+            nextButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(javafx.event.ActionEvent event) {
+                    String answer = ((JFXTextField) lookup("#recover_answer")).getText();
+
+                    if (qNum == 1 && answer.equalsIgnoreCase(user[4])) { //if answer to q1 is correct
+                        lookup("#login_error").setVisible(false);
+                        if (user[5] != null) { //see if there is a second question
+                            user[3] = "null";
+                            qNum = 2;
+                            switchScreen(RecoverQuestionScene.class);//ask that question
+                        } else { //only one question
+                            qNum = 1;
+                            user = null;
+                            switchScreen(RecoverCodeScene.class);//send code
+                        }
+                    } else if (qNum == 2 && answer.equalsIgnoreCase(user[6])) {//if answer to q2 is correct
+                        lookup("#login_error").setVisible(false);
+                        qNum = 1;
+                        user = null;
+                        switchScreen(RecoverCodeScene.class); //send code
+                    } else { //incorrect password
+                        lookup("#login_error").setVisible(true);
+                    }
+                }
+            });
+
+        } else { //no security questions
+            qNum = 1;
+            user = null;
+            switchScreen(RecoverCodeScene.class); //send code
+        }
+    }
+
+    @Override
+    public void onDispose() {
+
+    }
 }
