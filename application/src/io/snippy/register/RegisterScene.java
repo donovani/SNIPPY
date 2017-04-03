8ae2cc9ad786642009227b26660fcdf903994787
Jake Hall <hallja99@gmail.com>
2017-04-03 01:11:21 -0400

    Logic changes - changed logic that was causing a user to register if security answer 1 was not blank but the question was - implemented debug specific methods

--- application/src/io/snippy/register/RegisterScene.java
+++ application/src/io/snippy/register/RegisterScene.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
  * Created by Ian on 2/18/2017.
  */
 public class RegisterScene extends StageScene {
-
     public RegisterScene(Stage primaryStage) {
         super(primaryStage);
     }
@@ -61,74 +60,154 @@ public class RegisterScene extends StageScene {
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(javafx.event.ActionEvent event) {
-                String email = "";
-                String name = "";
-                String pass1 = "";
-                String pass2 = "";
-                String sec1 = "";
-                String seca1 = "";
-                String sec2 = "";
-                String seca2 = "";
-                try {
-                    email = ((JFXTextField) lookup("#register_email")).getText();
-                    name = ((JFXTextField) lookup("#register_name")).getText();
-                    pass1 = ((JFXPasswordField) lookup("#register_password")).getText();
-                    pass2 = ((JFXPasswordField) lookup("#register_confirm")).getText();
-                    sec1 = ((JFXComboBox) lookup("#register_question1")).getValue().toString();
-                    seca1 = ((JFXTextField) lookup("#register_answer1")).getText();
-                    sec2 = ((JFXComboBox) lookup("#register_question2")).getValue().toString();
-                    seca2 = ((JFXTextField) lookup("#register_answer2")).getText();
-                } catch (Exception e) {
 
-                }
-                if (email.equals("")) { //no email visible
+                String email = getValue((JFXTextField) lookup("#register_email"));
+                String name = getValue((JFXTextField) lookup("#register_name"));
+                String pass1 = getValue((JFXPasswordField) lookup("#register_password"));
+                String pass2 = getValue((JFXPasswordField) lookup("#register_confirm"));
+                String sec1 = getValue((JFXComboBox) lookup("#register_question1"));
+                String seca1 = getValue((JFXTextField) lookup("#register_answer1"));
+                String sec2 = getValue((JFXComboBox) lookup("#register_question2"));
+                String seca2 = getValue((JFXTextField) lookup("#register_answer2"));
+
+                //DEBUG
+                println("\n\n==========");
+                println("Email: " + email);
+                println("Name: " + name);
+                println("Password: " + pass1);
+                println("Password (Conf): " + pass2);
+                println("Security Question 1: " + sec1);
+                println("Security Answer 1: " + seca1);
+                println("Security Question 2: " + sec2);
+                println("Security Answer 2: " + seca2);
+                println("==========\n\n");
+                //END DEBUG
+
+                if (email.equals("") || email.indexOf("@") == -1) { //no email visible
                     hideErrors();
                     lookup("#register_error1").setVisible(true);
+
+                    printErr("Email issue");
                 } else if (SQLUtils.userExists(email)) {
                     hideErrors();
                     lookup("#register_error5").setVisible(true);
+
+                    println("Email has already been registered");
                 } else if (pass1.equals("") || pass2.equals("")) { //a password was empty
                     hideErrors();
                     lookup("#register_error2").setVisible(true);
+
+                    println("A password field was blank");
                 } else if (!pass1.equals(pass2)) { //passwords dont match
                     hideErrors();
                     lookup("#register_error3").setVisible(true);
+
+                    println("Passwords do not match");
                 } else if ((sec1.equals("") && !seca1.equals("")) || (!sec1.equals("") && seca1.equals(""))) {//if sec q or answer are empty while the other is filled out
                     hideErrors();
                     lookup("#register_error4").setVisible(true);
+
+                    println("Security question 1 is blank or response is blank");
                 } else if ((sec2.equals("") && !seca2.equals("")) || (!sec2.equals("") && seca2.equals(""))) {//if sec q or answer are empty while the other is filled out
                     hideErrors();
                     lookup("#register_error4").setVisible(true);
-                } else {
-                    String salt = SQLUtils.generateSalt();
-                    String pass = SQLUtils.hash(pass1, salt);
 
-                    System.out.println("HELLO");
-                    boolean success = SQLUtils.createUser(pass, email, name, sec1, seca1, sec2, seca2, salt);
+                    println("Security question 2 is blank or response is blank");
+                } else {
+                    String salt = SQLUtils.generateSalt(); //generate salt
+                    String pass = SQLUtils.hash(pass1, salt); //hash the user's password
+
+                    println("Salt: " + salt);
+                    println("Hashed and Salted: " + pass);
+
+                    boolean success = SQLUtils.createUser(pass, email, name, sec1, seca1, sec2, seca2, salt); //try creating an account
 
                     if (success) {
-                        switchScreen(LoginScene.class);
-                    } else {
+                        hideErrors();
+                        lookup("#register_success").setVisible(true);
+
+                        switchScreen(LoginScene.class); //go back to login scene
+                    } else { //cannot create the account
                         hideErrors();
                         lookup("#register_error6").setVisible(true);
+
+                        println("There was an issue creating the account");
                     }
                 }
             }
         });
-
     }
 
-    public void hideErrors() {
+    private String getValue(JFXTextField field) {//look up the value
+        String val = "";
+        try {//set the values to ones in the fields
+            val = field.getText();
+        } catch (Exception e) {
+            printErr(e);
+        }
+        return val;
+    }
+
+    private String getValue(JFXComboBox field) {//look up the value
+        String val = "";
+        try {//set the values to ones in the fields
+            val = field.getValue().toString();
+        } catch (Exception e) {
+            printErr(e);
+        }
+        return val;
+    }
+
+    private String getValue(JFXPasswordField field) {//look up the value
+        String val = "";
+        try {//set the values to ones in the fields
+            val = field.getText();
+        } catch (Exception e) {
+            printErr(e);
+        }
+        return val;
+    }
+
+    public void hideErrors() { //make all error messages invisible
         lookup("#register_error1").setVisible(false);
         lookup("#register_error2").setVisible(false);
         lookup("#register_error3").setVisible(false);
         lookup("#register_error4").setVisible(false);
         lookup("#register_error5").setVisible(false);
         lookup("#register_error6").setVisible(false);
+        lookup("#register_success").setVisible(false);
     }
 
     @Override
     public void onDispose() {
 
+    }
+
+    //============DEBUG=============================
+    private boolean debug = true;
+
+    private void print(String val) {
+        if (debug) {
+            System.out.print(val);
+        }
+    }
+
+    private void println(String val) {
+        if (debug) {
+            print(val + "\n");
+        }
+    }
+
+    private void printErr(String err) {
+        if (debug) {
+            System.err.println(err);
+        }
+    }
+
+    private void printErr(Exception e) {
+        if (debug) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
