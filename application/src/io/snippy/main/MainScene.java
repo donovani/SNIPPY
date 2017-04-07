0d2de86e5e9cb8c254dd4e1e219cc9ad1373a4bd
Jake Hall <hallja99@gmail.com>
2017-04-07 10:53:42 -0400

    Disable share on new snip

--- application/src/io/snippy/main/MainScene.java
+++ application/src/io/snippy/main/MainScene.java
@@ -5,6 +5,7 @@ import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import io.snippy.core.R;
 import io.snippy.core.Snip;
 import io.snippy.core.StageScene;
+import io.snippy.core.User;
 import io.snippy.login.LoginScene;
 import io.snippy.util.Language;
 import io.snippy.util.SQLUtils;
@@ -32,165 +33,186 @@ import static io.snippy.util.Language.SQL;
  */
 public class MainScene extends StageScene {
 
-	private JFXHamburger menuButton;
-	private HamburgerBasicCloseTransition closeTransition;
+    private JFXHamburger menuButton;
+    private HamburgerBasicCloseTransition closeTransition;
 
-	private JFXListView<Parent> snips;
+    private JFXListView<Parent> snips;
 
-	private ArrayList<Snip> userSnips = new ArrayList<Snip>();
-	private Snip displayedSnip;
+    private ArrayList<Snip> userSnips = new ArrayList<Snip>();
+    private Snip displayedSnip;
 
-	public MainScene( Stage primaryStage ) {
-		super( primaryStage );
-	}
+    public MainScene(Stage primaryStage) {
+        super(primaryStage);
+    }
 
-	@Override
-	public Parent inflateLayout() {
-		return UXUtils.inflate( "assets/layouts/layout_base.fxml" );
-	}
+    @Override
+    public Parent inflateLayout() {
+        return UXUtils.inflate("assets/layouts/layout_base.fxml");
+    }
 
-	@Override
-	public void onCreate() {
-		//First we want to load the rest of the main scene
-		Pane contentRoot = (Pane) lookup( "#base_content" );
-		contentRoot.getChildren().add( UXUtils.inflate( "assets/layouts/main_content.fxml" ) );
+    @Override
+    public void onCreate() {
+        //First we want to load the rest of the main scene
+        Pane contentRoot = (Pane) lookup("#base_content");
+        contentRoot.getChildren().add(UXUtils.inflate("assets/layouts/main_content.fxml"));
 
-		//Then we add some listener junk
-		menuButton = (JFXHamburger) this.lookup( "#base_menu" );
-		closeTransition = new HamburgerBasicCloseTransition( menuButton );
-		closeTransition.setRate( -3 );
-		menuButton.addEventHandler( MouseEvent.MOUSE_PRESSED, (e)->{
-			closeTransition.setRate(closeTransition.getRate() * -1);
-			closeTransition.play();
-		});
+        //Then we add some listener junk
+        menuButton = (JFXHamburger) this.lookup("#base_menu");
+        closeTransition = new HamburgerBasicCloseTransition(menuButton);
+        closeTransition.setRate(-3);
+        menuButton.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+            closeTransition.setRate(closeTransition.getRate() * -1);
+            closeTransition.play();
+        });
 
-		//Now we setup events for dialogs
-		StackPane overlay = (StackPane) this.lookup( "#base_stack" );
-		JFXButton deleteButton = (JFXButton) this.lookup( "#main_delete" );
-		String snipID = "<snip uuid goes here>";
-		Logger.info( "Creating deletion dialog for SnipID {}", snipID);
-		deleteButton.setOnAction( event -> DeleteDialog.createAndShow( overlay, snipID ) );
+        //Now we setup events for dialogs
+        StackPane overlay = (StackPane) this.lookup("#base_stack");
+        JFXButton deleteButton = (JFXButton) this.lookup("#main_delete");
+        String snipID = "<snip uuid goes here>";
+        Logger.info("Creating deletion dialog for SnipID {}", snipID);
+        deleteButton.setOnAction(event -> DeleteDialog.createAndShow(overlay, snipID));
 
-		//Instantiating the Lnaguage Combobox
-		JFXComboBox languageDropdown = ((JFXComboBox) lookup("#main_language"));
-		ArrayList<String> languageOptions = new ArrayList<String>();
-		for (Language l : Language.values()){
-			languageOptions.add(l.NAME);
-		}
-		languageDropdown.getItems().addAll(languageOptions);
+        //Instantiating the Language Combobox
+        JFXComboBox languageDropdown = ((JFXComboBox) lookup("#main_language"));
+        ArrayList<String> languageOptions = new ArrayList<String>();
+        for (Language l : Language.values()) {
+            languageOptions.add(l.NAME);
+        }
+        languageDropdown.getItems().addAll(languageOptions);
 
-		//Creating some dummy snips for testing, will delete later
-		for (int i=0; i<=50; i++){
-			userSnips.add(new Snip("Snip"+i, "test", "Python"));
-			SQLUtils.createSnip(LoginScene.currentUser.getUserId(), "Snip"+i, "Python",  "test");
-		}
-		displayMainSnip();
-		displaySideSnips();
+        //Creating some dummy snips for testing, will delete later
+        for (int i = 0; i <= 50; i++) {
+            userSnips.add(new Snip("Snip" + i, "test", "Python"));
+            //SQLUtils.createSnip(LoginScene.currentUser.getUserId(), "Snip"+i, "Python",  "test");
+        }
+        displayMainSnip();
+        displaySideSnips();
 
-		JFXButton newButton = (JFXButton) lookup("#base_new");
-		newButton.setOnAction(event -> createNewSnip());
-	}
+        JFXButton newButton = (JFXButton) lookup("#base_new");
+        newButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                JFXButton share = (JFXButton) lookup("#main_share");
+                share.setStyle("-fx-background-color: #A9A9A9");
+                share.setDisable(true);
+                createNewSnip();
+            }
+        });
+    }
+    /*
+    NEED TO ADD THE FOLLOWING TO THE ON CLICK FOR SIDE BAR SNIPS
+    JFXButton share = (JFXButton) lookup("#main_share");
+    share.setStyle("-fx-background-color: #44aaff");
+    share.setDisable(true);
+     */
 
-	public void displayMainSnip(){
-		Snip mostRecentSnip = userSnips.get(userSnips.size()-1);
-		System.out.println(mostRecentSnip.toString());
-		((JFXTextField) lookup("#main_title")).setText(mostRecentSnip.getTitle());
-		((TextArea) lookup("#main_code")).setText(mostRecentSnip.getCodeSnippet());
-		((JFXComboBox) lookup("#main_language")).getSelectionModel().select(mostRecentSnip.getLanguage());
-	}
+    public void displayMainSnip() {
+        Snip mostRecentSnip = userSnips.get(userSnips.size() - 1);
+        System.out.println(mostRecentSnip.toString());
+        ((JFXTextField) lookup("#main_title")).setText(mostRecentSnip.getTitle());
+        ((TextArea) lookup("#main_code")).setText(mostRecentSnip.getCodeSnippet());
+        ((JFXComboBox) lookup("#main_language")).getSelectionModel().select(mostRecentSnip.getLanguage());
+    }
 
-	public void displaySideSnips(){
-		snips = (JFXListView< Parent >) lookup( "#base_selections" );
-		for (int i=49; i>=0; i--) {
-			snips.getItems().add(new SnipListData().toNode(userSnips.get(i)));
-		}
-	}
+    public void displaySideSnips() {
+        snips = (JFXListView<Parent>) lookup("#base_selections");
+        for (int i = 49; i >= 0; i--) {
+            snips.getItems().add(new SnipListData().toNode(userSnips.get(i)));
+        }
+    }
 
-	public void createNewSnip() {
-		//Clear title and add prompt text
-		JFXTextField snipTitle = ((JFXTextField) lookup("#main_title"));
-		snipTitle.setText(null);
-		snipTitle.setPromptText("Enter Snip Title");
+    public void createNewSnip() {
+        //Clear title and add prompt text
+        JFXTextField snipTitle = ((JFXTextField) lookup("#main_title"));
+        snipTitle.setText(null);
+        snipTitle.setPromptText("Enter Snip Title");
 
-		//Clear code area and add prompt text
-		TextArea codeText = ((TextArea) lookup("#main_code"));
-		codeText.setText(null);
-		codeText.setPromptText("Enter your code here.");
+        //Clear code area and add prompt text
+        TextArea codeText = ((TextArea) lookup("#main_code"));
+        codeText.setText(null);
+        codeText.setPromptText("Enter your code here.");
 
-		//Clear dropdown and add language options
-		JFXComboBox languageDropdown = ((JFXComboBox) lookup("#main_language"));
-		languageDropdown.getSelectionModel().select(0);
-
-
-		JFXButton saveButton = (JFXButton) lookup("#main_save");
-		saveButton.setOnAction(new EventHandler<ActionEvent>() { // on click
-			@Override
-			public void handle(ActionEvent event) {
-
-				String snipTitle = ((JFXTextField) lookup("#main_title")).getText();
-				String snipCode = ((TextArea) lookup ("#main_code")).getText();
-				String snipLanguage = ((JFXComboBox) lookup("#main_language")).getSelectionModel().getSelectedItem().toString();
-				boolean readyToCreate = true;
-
-				if(!(snipTitle != null && !snipTitle.equals(""))) {
-					((JFXTextField) lookup("#main_title")).setStyle("-fx-prompt-text-fill: rgba(255, 0, 0, 1)");
-					readyToCreate = false;
-				}
-				if (!(snipCode != null && !snipCode.equals(""))) {
-					((TextArea) lookup("#main_code")).setStyle("-fx-prompt-text-fill: rgba(255, 0, 0, 1)");
-					readyToCreate = false;
-				}
-				if(readyToCreate){
-					((JFXTextField) lookup("#main_title")).setStyle("-fx-prompt-text-fill: rgba(0, 0, 0, 1)");
-					((TextArea) lookup("#main_code")).setStyle("-fx-prompt-text-fill: rgba(0, 0, 0, 1)");
-					System.out.println(SQLUtils.createSnip(LoginScene.currentUser.getUserId(),snipTitle, snipCode));
-					userSnips.add(new Snip(snipTitle, snipCode, snipLanguage));
+        //Clear dropdown and add language options
+        JFXComboBox languageDropdown = ((JFXComboBox) lookup("#main_language"));
+        languageDropdown.getSelectionModel().select(0);
 
 
-				}
-			}
-		});
+        JFXButton saveButton = (JFXButton) lookup("#main_save");
+        saveButton.setOnAction(new EventHandler<ActionEvent>() { // on click
+            @Override
+            public void handle(ActionEvent event) {
 
-	}
+                String snipTitle = ((JFXTextField) lookup("#main_title")).getText();
+                String snipCode = ((TextArea) lookup("#main_code")).getText();
+                String snipLanguage = ((JFXComboBox) lookup("#main_language")).getSelectionModel().getSelectedItem().toString();
+                boolean readyToCreate = true;
 
-	@Override
-	public void onDispose() {
+                if (!(snipTitle != null && !snipTitle.equals(""))) {
+                    ((JFXTextField) lookup("#main_title")).setStyle("-fx-prompt-text-fill: rgba(255, 0, 0, 1)");
+                    readyToCreate = false;
+                }
+                if (!(snipCode != null && !snipCode.equals(""))) {
+                    ((TextArea) lookup("#main_code")).setStyle("-fx-prompt-text-fill: rgba(255, 0, 0, 1)");
+                    readyToCreate = false;
+                }
+                if (readyToCreate) {
+                    ((JFXTextField) lookup("#main_title")).setStyle("-fx-prompt-text-fill: rgba(0, 0, 0, 1)");
+                    ((TextArea) lookup("#main_code")).setStyle("-fx-prompt-text-fill: rgba(0, 0, 0, 1)");
+                    System.out.println(SQLUtils.createSnip(LoginScene.currentUser.getUserId(), snipTitle, snipCode));
+                    userSnips.add(new Snip(snipTitle, snipCode, snipLanguage));
 
-	}
+                    JFXButton share = (JFXButton) lookup("#main_share");
+                    share.setStyle("-fx-background-color: #44aaff");
+                    share.setDisable(false);
+                }
+            }
+        });
+
+    }
+
+    public void shareSnip() {
+        if (displayedSnip.getID() != -1) {
+
+        }
+    }
+
+    @Override
+    public void onDispose() {
+
+    }
 }
-
 
 
 class DeleteDialog extends JFXDialog {
 
-	private final StackPane stackPane;
-	private final String snipID;
-	private JFXDialogLayout layout;
-	private JFXButton cancelButton, confirmButton;
+    private final StackPane stackPane;
+    private final String snipID;
+    private JFXDialogLayout layout;
+    private JFXButton cancelButton, confirmButton;
 
-	private DeleteDialog( StackPane stackPane, String snipID ) {
-		this.stackPane = stackPane;
-		this.snipID = snipID;
-		this.layout = new JFXDialogLayout();
-		create( );
-	}
+    private DeleteDialog(StackPane stackPane, String snipID) {
+        this.stackPane = stackPane;
+        this.snipID = snipID;
+        this.layout = new JFXDialogLayout();
+        create();
+    }
 
-	private final void create( ) {
-		cancelButton = new JFXButton( R.strings( "common.cancel" ) );
-		confirmButton = new JFXButton( R.strings( "delete.confirm" ) );
-		confirmButton.setStyle( "-fx-text-fill: WHITE; -fx-background-color: RED;" );
+    private final void create() {
+        cancelButton = new JFXButton(R.strings("common.cancel"));
+        confirmButton = new JFXButton(R.strings("delete.confirm"));
+        confirmButton.setStyle("-fx-text-fill: WHITE; -fx-background-color: RED;");
 
-		layout.setHeading( new Label( R.strings( "delete.header" ) ) );
-		layout.setBody( new Label( R.strings( "delete.body" ) ) );
-		layout.setActions( cancelButton, confirmButton );
+        layout.setHeading(new Label(R.strings("delete.header")));
+        layout.setBody(new Label(R.strings("delete.body")));
+        layout.setActions(cancelButton, confirmButton);
 
-		this.setTransitionType( DialogTransition.CENTER );
-		this.setContent( layout );
-	}
+        this.setTransitionType(DialogTransition.CENTER);
+        this.setContent(layout);
+    }
 
-	public static final JFXDialog createAndShow( StackPane sp, String id ) {
-		JFXDialog d = new DeleteDialog(sp, id);
-		d.show(sp);
-		return d;
-	}
+    public static final JFXDialog createAndShow(StackPane sp, String id) {
+        JFXDialog d = new DeleteDialog(sp, id);
+        d.show(sp);
+        return d;
+    }
 }
